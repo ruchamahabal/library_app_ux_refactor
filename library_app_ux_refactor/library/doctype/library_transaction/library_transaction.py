a7a6173b298a7717a29160a010cc2afaa4e14624
Rucha Mahabal <ruchamahabal2@gmail.com>
2024-03-07 14:49:48 +0530

    chore: add a bad validation message 🫠

--- library_app_ux_refactor/library/doctype/library_transaction/library_transaction.py
+++ library_app_ux_refactor/library/doctype/library_transaction/library_transaction.py
@@ -1,9 +1,14 @@
 # Copyright (c) 2024, Rucha Mahabal and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class LibraryTransaction(Document):
-	pass
+	def validate(self):
+		if not frappe.db.get_value(
+			"Library Membership",
+			{"member": self.member, "from_date": ("<=", self.date), "to_date": (">=", self.date)},
+		):
+			frappe.throw("No active membership")
